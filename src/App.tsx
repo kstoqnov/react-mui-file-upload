28b0d339db5e32dee856db93cb9c48032ceea19c
kstoqnov <kirilstoqnov@gmail.com>
2022-03-03 10:50:53 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import "./styles.css";
+import React from 'react';
+import { FileUpload, FileUploadProps } from "./FileUpload/FileUpload";
+
+const fileUploadProp: FileUploadProps = {
+  accept: 'image/*',
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => {
+      if (
+          event.target.files !== null &&
+          event.target?.files?.length > 0
+      ) {
+          console.log(`Saving ${event.target.value}`)
+      }
+  },
+  onDrop: (event: React.DragEvent<HTMLElement>) => {
+      console.log(`Drop ${event.dataTransfer.files[0].name}`)
+  },
+}
+
+export default function App() {
+  return (
+    <div className="App">
+      {/* <FileUpload {...fileUploadProp} imageButton/> */}
+      <FileUpload {...fileUploadProp} />
+    </div>
+  );
+}
